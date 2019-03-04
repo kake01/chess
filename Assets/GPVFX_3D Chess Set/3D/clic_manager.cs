58b82224246d293ac5b7d9f5d9c9f90948afbadb
ikuma70 <kakeruww70@gmail.com>
2019-03-04 14:32:20 +0900

    pawnの移動の実装完了。 click_managerとboardの盤面状況の更新を書けば。8割は完成 これからの見通し 　1,boardの更新を書く 　2,白の駒の移動を全て書く 　3,白と黒のscriptを分けて書く必要はないので、turnで制御出来る様に改良

--- Assets/GPVFX_3D Chess Set/3D/clic_manager.cs
+++ Assets/GPVFX_3D Chess Set/3D/clic_manager.cs
@@ -6,9 +6,10 @@ public class clic_manager : MonoBehaviour
 {
     //private bool turn = true;
     //こいつここ?
-    public bool is_select_time = false;
+    private RaycastHit first_hit;
+    private bool is_select_time = false;
     private int cell_length = 125;
-    public int select_x, select_y;
+    private int select_x, select_y, move_pos_x, move_pos_y;
     public GameObject board;
     public int[,] board_state;
 
@@ -21,22 +22,23 @@ public class clic_manager : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            //カメラから光線を飛ばす準備
-            Ray first_ray = new Ray();
-            first_ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit first_hit = new RaycastHit();
+            board_state = board.GetComponent<board>().get_board_2D_array();
 
-            //マウスクリックした場所にRayを飛ばし、オブジェクトがあればtrue
-            if (Physics.Raycast(first_ray.origin, first_ray.direction, out first_hit, Mathf.Infinity))
+            if (!is_select_time)
             {
-                //getしたオブジェクトの座標を取得
-                Vector3 worldDir = first_hit.transform.position;
-                select_y = -(int)worldDir.z / cell_length;
-                select_x = (int)worldDir.x / cell_length;
-                board_state = board.GetComponent<board>().get_board_2D_array();
+                //カメラから光線を飛ばす準備
+                Ray first_ray = new Ray();
+                first_ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                first_hit = new RaycastHit();
 
-                if (!is_select_time)
+                //マウスクリックした場所にRayを飛ばし、オブジェクトがあればtrue
+                if (Physics.Raycast(first_ray.origin, first_ray.direction, out first_hit, Mathf.Infinity))
                 {
+                    Vector3 worldDir = first_hit.transform.position;
+                    //boardの二次元座標に変換
+                    select_y = -(int)worldDir.z / cell_length;
+                    select_x = (int)worldDir.x / cell_length;
+
                     first_hit.collider.gameObject.GetComponent<Renderer>().material.color = Color.red;
                     //クリックmanagerが盤面の情報を引数にして、駒の移動範囲のshowを呼び出す
                     switch (board_state[select_y, select_x])
@@ -65,60 +67,46 @@ public class clic_manager : MonoBehaviour
                     }
                     is_select_time = true;
                 }
-                else
-                {
-                    //カメラから光線を飛ばす準備
-                    Ray second_ray = new Ray();
-                    RaycastHit second_hit = new RaycastHit();
-                    second_ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-
-                    //マウスクリックした場所にRayを飛ばし、オブジェクトがあればtrue
-                    if (Physics.Raycast(second_ray.origin, second_ray.direction, out second_hit, Mathf.Infinity))
-                    {
-                        //当たったオブジェクトがplanなら
-                        /*if (second_hit.collider.tag == "play"){}*/
-
-                        //getしたオブジェクトの座標を取得
-                        Vector3 worldDir = second_hit.transform.position;
-                        select_y = -(int)worldDir.z / cell_length;
-                        select_x = (int)worldDir.x / cell_length;
-
-
-
-                        //もし、移動可能場所に触ったら
-                        switch (board_state[select_y, select_x])
-                        {
-                            case 6:
-                                first_hit.collider.gameObject.GetComponent<white_pawn>().chessman_move(-1/*移動可能場所のx座標*/, 1/*移動可能場所のy座標*/);
-                                Debug.Log("ここにいきたよ");
-                                break;
-                            case 1:
-                                break;
-                            case 2:
-                                break;
-                            case 3:
-                                break;
-                            case 4:
-                                break;
-                            case 5:
-                                break;
-                            default:
-                                break;
-                        }
-                        //色を戻す手段をここに書く
-                    }
-                    is_select_time = false;
-                }
             }
+            else
+            {
+                Ray second_ray = new Ray();
+                RaycastHit second_hit = new RaycastHit();
+                second_ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            /*
-             * 追加する内容
-             * ray2を実装
-             * ray2が黄色いマットだった場合、その場所の座標を取得し、駒のメソッドを呼ぶ呼ぶ
-             */
+                //マウスクリックした場所にRayを飛ばし、オブジェクトがあればtrue
+                if (Physics.Raycast(second_ray.origin, second_ray.direction, out second_hit, Mathf.Infinity))
+                {
+                    //当たったオブジェクトがplanなら
+                    /*if (second_hit.collider.tag == "play"){}*/
 
-            //選択状態で移動出来る場所かの検知
+                    Vector3 plan = second_hit.transform.position;
+                    move_pos_y = -(int)plan.z / cell_length;
+                    move_pos_x = (int)plan.x / cell_length;
+
+                    switch (board_state[select_y, select_x])
+                    {
+                        case 6:
+                            first_hit.collider.gameObject.GetComponent<white_pawn>().chessman_move(-(select_x - move_pos_x), select_y - move_pos_y);
+                            break;
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                        case 5:
+                            break;
+                        default:
+                            break;
+                    }
+                    //色を戻す手段をここに書く
+                    //板を消す作業をここに書く
+                }
+                is_select_time = false;
+            }
         }
     }
 }
